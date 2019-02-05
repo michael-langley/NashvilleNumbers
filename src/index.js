43ac5214ac898d513cfcc307b7f79e2e7031174c
Michael Langley <michaellangley@Michaels-MacBook-Pro.local>
2019-02-05 18:31:30 -0500

    Min CSS perf test

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './bt.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
